33d412f8763ba2d5f259ddac72f23c0905ad01d0
Jesse Shawl <jesse@jesse.sh>
2023-01-08 12:10:17 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const createCircle = props => {
+  const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle")
+  for(let prop in props)
+    circle.setAttribute(prop, props[prop])
+  return circle
+}
+
+const deg = (x, y) => {
+  const rad = Math.atan2(y, x);
+  let degrees = rad * (180 / Math.PI) - 90;
+  if (degrees < 0) {
+    degrees += 360;
+  }
+  return degrees;
+};
+
+const Dial = (selector) => {
+  const element = document.querySelector(selector)
+  element.style.display = 'none'
+  const svg = document.createElementNS("http://www.w3.org/2000/svg","svg")
+  const dasharray = 320;
+  const dashoffset = dasharray * (element.value / (element.getAttribute("max")||100));
+  let progress = dashoffset / dasharray
+  const circle = createCircle({
+    class: 'progress',
+    cx: 65,
+    cy: 65,
+    r: 50,
+    "stroke-dasharray": dasharray,
+    "stroke-dashoffset": dashoffset,
+    "stroke-width": 5,
+    stroke: '#007AFF'
+  })
+  svg.appendChild(circle)
+  const knob = createCircle({
+    class: 'knob'
+  })
+  knob.style.transformOrigin = `65px 65px`
+  knob.style.transform = `rotate(${360 * progress}deg)`
+  svg.appendChild(knob)
+  element.parentElement.insertBefore(svg, element)
+  svg.addEventListener('click', (e) => {
+    const x = e.screenX - svg.getBoundingClientRect().left;
+    const y = e.screenY - svg.getBoundingClientRect().top;
+    const cx = circle.getAttribute("cx");
+    const cy = circle.getAttribute("cy");
+    const degrees = deg(x - cx, y - cy);
+    const value = degrees / 3.6;
+    element.value = value;
+  })
+
+  return {
+    element,
+    svg,
+    progress
+  }
+}
+
+export default Dial
---
+++ src/index.test.js
@@ -0,0 +1,68 @@
+/**
+ * @jest-environment jsdom
+ */
+
+ import Dial from './index'
+
+ const template = (props) => `<input type='range' ${props}/>`;
+ 
+ beforeEach(() => {
+   document.body.innerHTML = template();
+ });
+
+test('selecting an existing input[type="range"]', () => {
+  const dial = Dial('[type="range"]')
+  expect(dial.element).toBeTruthy();
+});
+
+test('replaces the range input with an SVG', () => {
+  const dial = Dial('[type="range"]')
+  expect(dial.element.style.display).toBe('none');
+  expect(dial.element.parentElement.querySelector("svg")).toBeTruthy()
+  expect(dial.svg.querySelector(".progress")).toBeTruthy()
+});
+
+test('the dial progress value matches the range value on instantiation', () => {
+  const dial = Dial('[type="range"]')
+  expect(dial.element.value).toBe("50") // default
+  expect(dial.progress).toBe(dial.element.value / 100 /* 0.5 */)
+  
+  // reset
+  document.body.innerHTML = template('value="10"')
+  const dial2 = Dial('[type="range"]')
+  expect(dial2.element.value).toBe("10")
+  expect(dial2.progress).toBe(0.1)
+
+  // reset
+  document.body.innerHTML = template('value="10" max="40"')
+  const dial3 = Dial('[type="range"]')
+  expect(dial3.progress).toBe(0.25)
+
+  // TODO support min values
+})
+
+test('the knob follows the progress', () => {
+  const dial = Dial('[type="range"]')
+  expect(dial.svg.querySelector(".knob")).toBeTruthy()
+  expect(dial.svg.querySelector(".knob").style.transform).toBe('rotate(180deg)')
+
+  // reset
+  document.body.innerHTML = template('value="10" max="40"')
+  const dial2 = Dial('[type="range"]')
+  expect(dial2.svg.querySelector(".knob").style.transform).toBe('rotate(90deg)')
+})
+
+test('clicking on the dial updates the range value and progress', () => {
+  const event = new MouseEvent('click',{
+    'view': window,
+    'bubbles': true,
+    'cancelable': true,
+    'screenX': 100,
+    'screenY': 100
+  })
+  const dial = Dial('[type="range"]')
+  dial.svg.dispatchEvent(event)
+  expect(Number(dial.element.value)).toBeGreaterThan(75)
+  expect(dial.svg.querySelector(".knob").style.transform).toBe('rotate(270deg)')
+  expect(Number(dial.progress)).toBeGreaterThan(.75)
+})
